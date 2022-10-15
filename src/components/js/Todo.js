a5e24394be9a865b3b6eeef6f2711b64012094be
Andras korosi <89639857+Drewcamper@users.noreply.github.com>
2022-10-15 16:53:00 +0200

    Add files via upload

---
+++ src/components/js/Todo.js
@@ -0,0 +1,33 @@
+import { Checkbox, IconButton, ListItem, Typography } from "@material-ui/core";
+import DeleteIcon from '@mui/icons-material/Delete';
+import '../css/todo.css'
+import React from "react";
+
+function Todo({ todo, toggleComplete, removeTodo }) {
+  function handleCheckboxClick() {
+    toggleComplete(todo.id);
+  }
+
+  function handleRemoveClick() {
+    removeTodo(todo.id);
+  }
+
+  return (
+    <ListItem style={{ display: "flex" }}>
+      <Checkbox checked={todo.completed} onClick={handleCheckboxClick} />
+      <Typography className='Listitems'
+        variant="body1"
+        style={{
+          textDecoration: todo.completed ? "line-through" : null
+        }}
+      >
+        {todo.task}
+      </Typography>
+      <IconButton onClick={handleRemoveClick}>
+         <DeleteIcon /> 
+      </IconButton>
+    </ListItem>
+  );
+}
+
+export default Todo;
