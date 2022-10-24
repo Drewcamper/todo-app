67a9644bcf8acb48bd74f8e490615da4d338a479
Andras korosi <89639857+Drewcamper@users.noreply.github.com>
2022-10-24 19:56:27 +0200

    Add files via upload

---
+++ build/precache-manifest.7bf377d8555094550263bd167ec12c98.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5cf4497e6be9b4a0032b",
+    "url": "/static/css/main.a6ccee36.chunk.css"
+  },
+  {
+    "revision": "5cf4497e6be9b4a0032b",
+    "url": "/static/js/main.ac2b027a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "dd89eb0967c53cce9ce9",
+    "url": "/static/js/2.068d55d8.chunk.js"
+  },
+  {
+    "revision": "ae5f59f1f005ef2f7a9322ec4ec6b20a",
+    "url": "/index.html"
+  }
+];
